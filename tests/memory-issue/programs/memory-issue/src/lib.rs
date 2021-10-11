f2192e32414247930fb12c3acbbc90593f6a5f0b
Henry-E <henry.elder@adaptcentre.ie>
2021-10-11 10:12:18 +0000

    error not yet replicated

--- tests/memory-issue/programs/memory-issue/src/lib.rs
+++ tests/memory-issue/programs/memory-issue/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -11,4 +12,59 @@ pub mod memory_issue {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(init,
+        mint::decimals = 6,
+        mint::authority = authority,
+        seeds = [b"this_mint".as_ref()],
+        bump,
+        payer = authority)]
+    pub this_mint: Box<Account<'info, Mint>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_1".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_1: Box<Account<'info, TokenAccount>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_2".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_2: Box<Account<'info, TokenAccount>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_3".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_3: Box<Account<'info, TokenAccount>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_4".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_4: Box<Account<'info, TokenAccount>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_5".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_5: Box<Account<'info, TokenAccount>>,
+    #[account(init,
+        token::mint = this_mint,
+        token::authority = authority,
+        seeds = [b"token_6".as_ref()],
+        bump,
+        payer = authority)]
+    pub token_6: Box<Account<'info, TokenAccount>>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
--- tests/memory-issue/tests/memory-issue.js
+++ tests/memory-issue/tests/memory-issue.js
@@ -1,4 +1,5 @@
 const anchor = require('@project-serum/anchor');
+const Token = require("@solana/spl-token");
 
 describe('memory-issue', () => {
 
@@ -8,7 +9,53 @@ describe('memory-issue', () => {
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.MemoryIssue;
-    const tx = await program.rpc.initialize();
+
+    const [thisMint] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("this_mint")],
+      program.programId
+    );
+
+    const [token1] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_1")],
+      program.programId
+    );
+    const [token2] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_2")],
+      program.programId
+    );
+    const [token3] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_3")],
+      program.programId
+    );
+    const [token4] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_4")],
+      program.programId
+    );
+    const [token5] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_5")],
+      program.programId
+    );
+    const [token6] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("token_6")],
+      program.programId
+    );
+
+    const tx = await program.rpc.initialize(
+      {
+        accounts: {
+          authority: program.provider.wallet.publicKey,
+          thisMint,
+          token1,
+          token2,
+          token3,
+          token4,
+          token5,
+          token6,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: Token.TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        }
+      });
     console.log("Your transaction signature", tx);
   });
 });
